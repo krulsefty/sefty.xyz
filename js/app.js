54165b89f1eed2a8c5617936428b21a013377442
sefty <68341055+krulsefty@users.noreply.github.com>
2023-11-03 23:29:22 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -146,18 +146,22 @@ document.onkeydown = function (e) {
     }
 
     if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank')    
         return false
     }
 
     if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank')
         return false
     }
 
     if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank')
         return false
     }
 
     if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank')
         return false
     }
 }
