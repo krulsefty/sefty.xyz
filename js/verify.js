65b49e2a28386f30c98ab4e9bb9c124dfbd9e229
sefty <68341055+krulsefty@users.noreply.github.com>
2024-02-03 22:11:23 +0100

    Update verify.js

--- js/verify.js
+++ js/verify.js
@@ -1,30 +1,30 @@
-  // document.addEventListener("contextmenu", function (e) {
-  //   e.preventDefault()
-  // })
+  document.addEventListener("contextmenu", function (e) {
+    e.preventDefault()
+  })
   
-  // document.onkeydown = function (e) {
-  //   if (event.keyCode == 123) {
-  //     return false
-  //   }
+  document.onkeydown = function (e) {
+    if (event.keyCode == 123) {
+      return false
+    }
   
-  //   if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
-  //     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")
-  //     return false
-  //   }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+      window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")
+      return false
+    }
   
-  //   if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
-  //     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")
-  //     return false
-  //   }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+      window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")
+      return false
+    }
   
-  //   if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
-  //     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")
-  //     return false
-  //   }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+      window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")
+      return false
+    }
   
-  //   if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
-  //     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")
-  //     return false
-  //   }
-  // }
+    if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+      window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")
+      return false
+    }
+  }
   
